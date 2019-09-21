9c60ed6ec7ab9b112d6d6fc6652f3f71d6d8f1a2
Michael DeSantis <desanmichael1@gmail.com>
2019-09-21 18:11:33 -0500

    Updates

---
+++ precache-manifest.88feaf665152dea3050012d1c405456c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9b30ade2e856647ebca04686884ab2",
+    "url": "/weather-application_react/index.html"
+  },
+  {
+    "revision": "bf71bfac7e33698314f5",
+    "url": "/weather-application_react/static/css/2.48ba3c4e.chunk.css"
+  },
+  {
+    "revision": "15c303ce03d6ace3964e",
+    "url": "/weather-application_react/static/css/main.739e6880.chunk.css"
+  },
+  {
+    "revision": "bf71bfac7e33698314f5",
+    "url": "/weather-application_react/static/js/2.0a0a589c.chunk.js"
+  },
+  {
+    "revision": "15c303ce03d6ace3964e",
+    "url": "/weather-application_react/static/js/main.67ea307f.chunk.js"
+  },
+  {
+    "revision": "2f92974c46ba54df9f12",
+    "url": "/weather-application_react/static/js/runtime~main.81c9af7b.js"
+  },
+  {
+    "revision": "872b2de2412be05da5c63ce497807d20",
+    "url": "/weather-application_react/static/media/lighthouse-weather.872b2de2.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-application_react/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-application_react/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-application_react/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-application_react/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-application_react/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
